e1dfbf4aa2d6c2fc024efa07bc8ccb1a4465fae5
Sergii.Shestakov <sshest78@gmail.com>
2022-07-24 17:37:03 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+import { Observable, Subscription, of, fromEvent, from, empty, merge, timer } from 'rxjs';
+import { map, mapTo, switchMap, tap, mergeMap, takeUntil, filter, finalize } from 'rxjs/operators';
+
+declare type RequestCategory = 'cats' | 'meats';
+
 import { Component } from '@angular/core';
 
 @Component({
